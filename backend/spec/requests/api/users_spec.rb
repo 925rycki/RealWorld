05c2b08a4de5fde900d15ca9441bbcf153488d77
925rycki <925rycki@gmail.com>
2023-06-14 14:55:05 +0900

    RuboCopをパスするようにした

--- backend/spec/requests/api/users_spec.rb
+++ backend/spec/requests/api/users_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Api::Users" do
   describe "POST /api/users" do
-    it 'POST api/usersした場合、HTTPステータスコード201が返ってくる' do
-      post api_users_path, params:
-      {
+    subject(:create_user) do
+      post api_users_path, params: {
         user: {
           username: "Jacob",
           email: "jake@jake.jake",
           password: "jakejake"
         }
       }
+    end
+
+    it 'api/usersにPOSTリクエストを送るとHTTPステータスコード201が返ること' do
+      expect { create_user }.to change(User, :count).by(1)
+    end
+
+    it 'api/usersにPOSTリクエストを送るとcreatedステータスが返ること' do
+      create_user
       expect(response).to have_http_status(:created)
     end
   end
