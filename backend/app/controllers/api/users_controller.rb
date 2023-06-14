27be4102281aa40581c0f9d36ff809ca770703dd
925rycki <925rycki@gmail.com>
2023-06-14 13:48:20 +0900

    users_controllerのcreateメソッドに例外処理を追加

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -39,35 +39,40 @@ module Api
     end
 
     def create
-      # ユーザの作成
-      user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
-      # ペイロードの作成
-      payload = {
-        iss: "example_app", # JWTの発行者
-        sub: user.id, # JWTの主体
-        exp: (DateTime.current + 14.days).to_i # JWTの有効期限
-      }
-
-      # 秘密鍵の取得
-      rsa_private = OpenSSL::PKey::RSA.new(Rails.root.join('auth/service.key').read)
-
-      # JWTの作成
-      token = JWT.encode(payload, rsa_private, "RS256")
-
-      # JWTをCookieにセット
-      cookies[:token] = token
-
-      response = {
-        user: {
-          email: user.email,
-          token:,
-          username: user.username,
-          bio: user.bio,
-          image: user.image
+      begin
+        # ユーザの作成
+        user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+        # ペイロードの作成
+        payload = {
+          iss: "example_app", # JWTの発行者
+          sub: user.id, # JWTの主体
+          exp: (DateTime.current + 14.days).to_i # JWTの有効期限
         }
-      }
-
-      render json: response, status: :created
+    
+        # 秘密鍵の取得
+        rsa_private = OpenSSL::PKey::RSA.new(Rails.root.join('auth/service.key').read)
+    
+        # JWTの作成
+        token = JWT.encode(payload, rsa_private, "RS256")
+    
+        # JWTをCookieにセット
+        cookies[:token] = token
+    
+        response = {
+          user: {
+            email: user.email,
+            token:,
+            username: user.username,
+            bio: user.bio,
+            image: user.image
+          }
+        }
+    
+        render json: response, status: :created
+      rescue => e
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
     end
+    
   end
 end
